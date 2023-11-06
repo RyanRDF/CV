0590aabe38abe4aecc7e937ffbba16ffc9dbd64a
RyanRDF <ryanrafael032@gmail.com>
2023-11-06 18:57:36 +0700

    Update fix

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 $host = 'localhost';
 $db = 'cv';
-$user = 'CV1';
-$pwd = '1234';
+$user = 'cv1';
+$pwd = 'cv1';
 
 $conn = mysqli_connect($host, $user, $pwd, $db); // true | false
 
 if (!$conn) {
   die('Gagal terhubung ke database'. mysqli_connect_error());
 }
-?>
+
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include_once("config.php");
 
 //ambil data dan simpan kedalam variabel result
 $result = mysqli_query($conn, "SELECT * FROM cv_data");
+$data = mysqli_fetch_array($result);
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +15,39 @@ $result = mysqli_query($conn, "SELECT * FROM cv_data");
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  <title>Curiculum Vitae</title>
+  <title class="fs-1">Curiculum Vitae</title>
 </head>
 <body>
+  <!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a  class="fs-1" >Curiculum Vitae</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-outline-success" type="submit" ><a href="edit.php">Update</a></button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- Body -->
+<div class="card">
+  <div class="card-body" class="fs-4">
+  <div class="card" style="width: 18rem;">
+  <img src="<?php $data['foto_path']?>"" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">My Picture</p>
+  </div>
+</div>
+    <h1 class="fs-3"><?php echo $data['nama'];?></h1>
+    <p class="fs-4"><?php echo $data['pendidikan'];?></p>
+    <p class="fs-4"><?php echo $data['alamat'];?></p>
+    <p class="fs-4">Keterampilan      : <?php echo $data['keterampilan'];?></p>
+    <p class="fs-4">Pengalaman Kerja  : <?php echo $data['pengalaman_kerja'];?></p>
+    <p class="fs-4">Kontak</p>
+    <p class="fs-4">Hp : <?php echo $data['telepon'];?> Email : <?php echo $data['email'];?></p>
+  </div>
+</div>
 </body>
 </html>
